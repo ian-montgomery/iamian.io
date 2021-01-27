1b40edc1e54fdb036bce18a1e1c5e26a81d923fe
Ian Montgomery <ianmontgomery@posteo.net>
2021-01-28 10:55:58 +1300

    add contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect  } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
 import Title from '../Title/Title';
-import Form from '../Contact/Form'
+import Form from '../Contact/Form';
 
 const Contact = () => {
   const { contact } = useContext(PortfolioContext);
-  const { cta, btn, email } = contact;
+  const { cta1, btnEmail, email } = contact;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
 
   return (
     <section id="contact">
@@ -15,41 +28,19 @@ const Contact = () => {
         <Title title="Contact" />
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
-            <p className="contact-wrapper__text">{cta}</p>
+            <p className="contact-wrapper__text">{cta1}</p>
             <a
               target="_blank"
               rel="noopener noreferrer"
               className="cta-btn cta-btn--about"
               href={email ? `mailto:${email}` : 'https://www.linkedin.com/in/ian-montgomery/'}
             >
-              {btn || "Let's Talk"}
+              {btnEmail || "Let's Talk"}
             </a>
-            {/* <div className="contact-form"> */}
-              {/* <p className="contact-wrapper__text">Or use the form below</p> */}
-              {/* <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-                <input type="hidden" name="form-name" value="contact" />
-                <p className="contact-wrapper__text">
-                  <label>
-                    Your Name: <br/><input type="text" name="name" />
-                  </label>
-                </p>
-                <p className="contact-wrapper__text">
-                  <label>
-                    Your Email: <br/><input type="email" name="email" />
-                  </label>
-                </p>
-                <p className="contact-wrapper__text">
-                  <label>
-                    Message: <br/><textarea name="message"></textarea>
-                  </label>
-                </p>
-                <p className="contact-wrapper__text">
-                  <button className="cta-btn cta-btn--about" type="submit">Send</button>
-                </p>
-              </form> */}
-              {/* <Form /> */}
-            </div>
-          {/* </div> */}
+          </div>
+        </Fade>
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={2000} distance="30px">
+          <Form />
         </Fade>
       </Container>
     </section>
