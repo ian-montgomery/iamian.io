8521e020bfcabf1537b627e2133cfb7b4638c4da
Ian Montgomery <ianmontgomery@posteo.net>
2021-01-28 10:56:33 +1300

    refine form component

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -1,76 +1,82 @@
-import React from 'react';
-import { navigate } from 'gatsby-link'
-
+import React, { useContext } from 'react';
+import { navigate } from 'gatsby-link';
+import PortfolioContext from '../../context/context';
 
 function encode(data) {
-    return Object.keys(data)
-      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-      .join('&')
-  }
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+}
 
 export default function Form() {
-    const [state, setState] = React.useState({})
-  
-    const handleChange = (e) => {
-      setState({ ...state, [e.target.name]: e.target.value })
-    }
-  
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      const form = e.target
-      fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: encode({
-          'form-name': form.getAttribute('name'),
-          ...state,
-        }),
-      })
-        // .then(() => navigate(form.getAttribute('action')))
-        .catch((error) => alert(error))
-    }
-  
-    return (
-        <form
-          name="contact"
-          method="post"
-          // action='#'
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out: <input name="bot-field" onChange={handleChange} />
-            </label>
-          </p>
-          <p className="contact-wrapper__text">
-            <label>
-              Your name:
-              <br />
-              <input type="text" name="name" onChange={handleChange} />
-            </label>
-          </p>
-          <p className="contact-wrapper__text">
-            <label>
-              Your email:
-              <br />
-              <input type="email" name="email" onChange={handleChange} />
-            </label>
-          </p>
-          <p className="contact-wrapper__text">
-            <label>
-              Message:
-              <br />
-              <textarea name="message" onChange={handleChange} />
-            </label>
-          </p>
-          <p className="contact-wrapper__text">
-            <button className="cta-btn cta-btn--about" type="submit">Send</button>
-          </p>
-        </form>
-    )
-  }
-  
+  const [state, setState] = React.useState({});
+  const { contact } = useContext(PortfolioContext);
+  const { cta2, btnContact } = contact;
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error));
+  };
+
+  return (
+    <div className="contact-wrapper">
+      <p className="contact-wrapper__text">{cta2}</p>
+      <form
+        name="contact"
+        method="post"
+        action="thanks"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
+        <p className="contact-wrapper__text">
+          <label>
+            Your name:
+            <br />
+            <input type="text" name="name" required onChange={handleChange} />
+          </label>
+        </p>
+        <p className="contact-wrapper__text">
+          <label>
+            Your email:
+            <br />
+            <input type="email" name="email" required onChange={handleChange} />
+          </label>
+        </p>
+        <p className="contact-wrapper__text">
+          <label>
+            Message:
+            <br />
+            <textarea name="message" required onChange={handleChange} />
+          </label>
+        </p>
+        <p className="contact-wrapper__text">
+          <button className="cta-btn cta-btn--contact" type="submit">
+            {btnContact}
+          </button>
+        </p>
+      </form>
+    </div>
+  );
+}
